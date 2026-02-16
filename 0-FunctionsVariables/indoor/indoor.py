3da0b9cc223183b26727121dea2fd5b0c34cefaf
nadaa <nada.souksavong@education.nsw.gov.au>
2026-02-16 23:10:41 +0000

    Upload completed faces.py

---
+++ 0-FunctionsVariables/indoor/indoor.py
@@ -0,0 +1,4 @@
+print('HELLO')
+print('this is cs50')
+print('50')
+
---
+++ 0-FunctionsVariables/indoor/playback/playback.py
@@ -0,0 +1,3 @@
+print('This...is...CS50...')
+print('This...is...our...week...on...functions...')
+print('Lets...implement...called...hello...')
